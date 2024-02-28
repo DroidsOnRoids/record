61aacb858a54e3a6f35bcb27449f8c3e88aa13f3
ptuzinek <przeo1996@gmail.com>
2024-02-28 17:35:57 +0100

    [iOS] resume recording while app is in the background

--- record_darwin/ios/Classes/RecorderIOS.swift
+++ record_darwin/ios/Classes/RecorderIOS.swift
@@ -12,7 +12,7 @@ func listInputs() throws -> [Device] {
 
 func listInputDevices() throws -> [AVAudioSessionPortDescription]? {
   let audioSession = AVAudioSession.sharedInstance()
-  let options: AVAudioSession.CategoryOptions = [.defaultToSpeaker, .allowBluetooth]
+  let options: AVAudioSession.CategoryOptions = [.defaultToSpeaker, .mixWithOthers, .allowBluetooth]
   
   do {
     try audioSession.setCategory(.playAndRecord, options: options)
@@ -46,7 +46,7 @@ private func setInput(_ config: RecordConfig) throws {
 extension AudioRecordingDelegate {
   func initAVAudioSession(config: RecordConfig) throws {
     let audioSession = AVAudioSession.sharedInstance()
-    let options: AVAudioSession.CategoryOptions = [.defaultToSpeaker, .allowBluetooth]
+    let options: AVAudioSession.CategoryOptions = [.defaultToSpeaker, .mixWithOthers, .allowBluetooth]
     
     do {
       try audioSession.setCategory(.playAndRecord, options: options)
